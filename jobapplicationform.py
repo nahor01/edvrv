917a6d0d611c879904006f79898296fa89153fc6
nahor01 <69587863+nahor01@users.noreply.github.com>
2025-07-05 00:05:33 +0530

    Commit changes

---
+++ jobapplicationform.py
@@ -0,0 +1,33 @@
+#form submission for digital marketing job application
+ab=input("what you are applying for")
+print(ab)
+bc=input("type experience in yrs")
+print(bc)
+cd=input("type your experience in months")
+print(cd)
+de=input("type your experience in days")
+print(de)
+ef=input("type your name here")
+print(ef)
+fg=input("type your age here")
+print(fg)
+gh=input("type your previous company name here")
+print(gh)
+hi=input("type your notice period here")
+print(hi)
+ij=input("type your current salary")
+print(ij)
+jk=input("type your salary expectation")
+print(jk)
+kl=input("type confirm")
+if kl=="confirm":
+    print("You are applying for",ab,
+      "Your experience in yrs",bc,
+      "Your experience in months", cd,
+      "Your experience in days",de,
+      "Your name",ef,
+      "Your age",fg,
+      "Your previous company name",gh,
+      "Your notice period is", hi,
+      "Your current salary is",ij,
+      "Your salary expectation is",jk)
